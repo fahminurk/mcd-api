5cfbf849f1a1058f60ed194997451dbd4b8dfd0d
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-11-27 21:28:32 +0700

    update: update collection user

--- src/db/user.ts
+++ src/db/user.ts
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   fullname: { type: String },
   password: { type: String, required: true, select: false },
+  isSuperAdmin: { type: Boolean, default: false },
 });
 
 export const UserModel = mongoose.model("User", UserSchema);
