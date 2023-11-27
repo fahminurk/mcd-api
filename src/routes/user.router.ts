8f019ee026d1ced57cb0d72da745c84277840375
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-11-27 21:29:18 +0700

    update: user router

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,7 +1,13 @@
 import express from "express";
-import { getAllUser } from "../controllers/user.controller";
+import {
+  createNewUser,
+  deleteUser,
+  getAllUser,
+} from "../controllers/user.controller";
 import { isAuthenticated } from "../middlewares/authorization";
 
 export default (router: express.Router) => {
   router.get("/users", isAuthenticated, getAllUser);
+  router.delete("/users/:id", deleteUser);
+  router.post("/users/new", createNewUser);
 };
